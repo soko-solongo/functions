57c391a7db68cf51aae0a78bcb0562231338d770
soko-solongo <mungs031@newschool.edu>
2026-03-30 23:09:54 -0400

    fixed typo

--- reading-time/scripts/content.js
+++ reading-time/scripts/content.js
@@ -31,3 +31,10 @@ paras.forEach(paragraph => {
     paragraph.style.color = "red";
 }
 )
+
+let container = `
+<div id="paragraph-counter">
+    You have ${paras.length} paragraphs in this page.
+</div>
+`
+document.body.insertAdjacentHTML("afterbegin", container);
