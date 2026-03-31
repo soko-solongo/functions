978849ea860823760db5d02537020936fc5cae3e
soko-solongo <mungs031@newschool.edu>
2026-03-31 12:24:03 -0400

    attempt to stop at 0

--- eye-rest/popup.js
+++ eye-rest/popup.js
@@ -4,6 +4,10 @@ document.getElementById("startbutton").addEventListener("click", function() {
         let time = parseInt(timer.innerText); //converting the string in the <p> to an integer number
         time--; //substracting 1 from the time (counting down)
         timer.innerText = time; //updating the display after substracting 1    
+
+        if (time === 0) {
+            clearInterval(interval);
+        }
         
     }, 1000)
 })
