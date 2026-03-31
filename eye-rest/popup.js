536d714f3b57419f787f8c95fa495f54ff7ce4f5
soko-solongo <mungs031@newschool.edu>
2026-03-31 12:11:36 -0400

    added setInterval()

--- eye-rest/popup.js
+++ eye-rest/popup.js
@@ -1,5 +1,7 @@
 document.getElementById("startbutton").addEventListener("click", function() {
-
+    setInterval(function() {
+        let timer = document.getElementById("timer");
+        let time = timer.innerText.split(":");
+    console.log("started")
+    }, 1000)
 })
-
-console.log("started")
