711556fde10aded5f80d71719e857193f7546eed
soko-solongo <mungs031@newschool.edu>
2026-03-31 12:29:42 -0400

    finallyy stoppss at 0

--- eye-rest/popup.js
+++ eye-rest/popup.js
@@ -1,13 +1,13 @@
 document.getElementById("startbutton").addEventListener("click", function() {
-    setInterval(function() {
-        let timer = document.getElementById("timer"); // refers to <p> in html file
-        let time = parseInt(timer.innerText); //converting the string in the <p> to an integer number
-        time--; //substracting 1 from the time (counting down)
-        timer.innerText = time; //updating the display after substracting 1    
+    let intervalId = setInterval(function() {
+         let timer = document.getElementById("timer"); // refers to <p> in html file
+         let time = parseInt(timer.innerText); //converting the string in the <p> to an integer number
+         time--; //substracting 1 from the time (counting down)
+         timer.innerText = time; //updating the display after substracting 1    
 
-        if (time === 0) {
-            clearInterval(interval);
-        }
-        
-    }, 1000)
+         if (time === 0) {
+             clearInterval(intervalId); //stoping the timer when it reaches 0
+         }
+         
+     }, 1000)
 })
