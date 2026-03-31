27773755904cad4506225319a365a6d8bd89b275
soko-solongo <mungs031@newschool.edu>
2026-03-31 12:22:00 -0400

    countdown is working but going minus numbers indefinetly

--- eye-rest/popup.js
+++ eye-rest/popup.js
@@ -1,7 +1,9 @@
 document.getElementById("startbutton").addEventListener("click", function() {
     setInterval(function() {
-        let timer = document.getElementById("timer");
-        let time = timer.innerText.split(":");
-    console.log("started")
+        let timer = document.getElementById("timer"); // refers to <p> in html file
+        let time = parseInt(timer.innerText); //converting the string in the <p> to an integer number
+        time--; //substracting 1 from the time (counting down)
+        timer.innerText = time; //updating the display after substracting 1    
+        
     }, 1000)
 })
