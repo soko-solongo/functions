f827f40966f9a2931c18957f945777195c4ed3e2
soko-solongo <mungs031@newschool.edu>
2026-04-07 14:13:18 -0400

    realized I need service_worker

---
+++ eye-rest/scripts/background.js
@@ -0,0 +1,12 @@
+// let paras = document.querySelectorAll("p");
+// paras.forEach(paragraph => {
+//     // paragraph.style.filter = "blur(5px)";
+//     paragraph.style.color = "blue";
+// }
+// )
+
+// let container = `
+//     <div id="blur-effect">
+//     </div>
+//     `
+// document.body.insertAdjacentHTML("afterbegin", container);
